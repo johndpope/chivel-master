cfa9fb8161664b2e4d2cea006630f825b190eed9
Admin <support@unlimitpotential.com>
2022-09-09 01:53:09 -0400

    no message

--- utils/configs.ts
+++ utils/configs.ts
@@ -3,7 +3,7 @@ export default function getConfigs() {
     loginUrl: process.env.NEXT_PUBLIC_LOGIN_URL || "/auth/login",
     baseUrl:
       process.env.NEXT_PUBLIC_BASE_URL ||
-      "http://unlimitednowlive.vercel.app" ||
+      "https://unlimitednowlive.vercel.app" ||
       "unlimitednowlive.vercel.app",
   };
 }
